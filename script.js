d5a7386feb70c64f456c4ea63a6f9678a1a72d5a
Priya Krishnan <spriyak02@gmail.com>
2025-01-08 09:18:38 +0530

    Added some tables

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+import {Low} from "lowdb";
+import { JSONFile } from "lowdb/node";
+import lodash from "lodash";
 
+const defaultData = {
+    users: [], //for user info
+    tasks: [], //for all the tasks
+    lists: [], //for the lists that a task would be categorised into
+    tags: [], //for tags that would be assigned to a task
+    taskTags: [] //junction table for the many to many relationships between tags and tasks
+}
+
+const adapter = new JSONFile('db.json');
+const db = new Low(adapter, defaultData);
