34a7864865e2db02a58c45908fae509afa1bbcc3
Priya Krishnan <spriyak02@gmail.com>
2025-01-26 22:05:42 +0530

    added the modal and functionality to open modal

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-import {Low} from "lowdb";
-import { JSONFile } from "lowdb/node";
-import lodash from "lodash";
+// import {Low} from "lowdb";
+// import { JSONFile } from "lowdb/node";
+// import lodash from "lodash";
 
-const defaultData = {
-    users: [], //for user info
-    tasks: [], //for all the tasks
-    lists: [], //for the lists that a task would be categorised into
-    tags: [], //for tags that would be assigned to a task
-    taskTags: [] //junction table for the many to many relationships between tags and tasks
-}
+// const defaultData = {
+//     users: [], //for user info
+//     tasks: [], //for all the tasks
+//     lists: [], //for the lists that a task would be categorised into
+//     tags: [], //for tags that would be assigned to a task
+//     taskTags: [] //junction table for the many to many relationships between tags and tasks
+// }
 
-const adapter = new JSONFile('db.json');
-const db = new Low(adapter, defaultData);
+// const adapter = new JSONFile('db.json');
+// const db = new Low(adapter, defaultData);
+
+function openModal() {
+    document.getElementById('modal').style.display = 'block';
+    // document.getElementById('modalOverlay').style.display = 'block';
+  }
+
