68c10e0f0b7d465e523c1889007ab2879c50eb12
priyality <spriyak.work@gmail.com>
2025-02-04 16:39:57 +0530

    modal changes, form input changes

--- script.js
+++ script.js
@@ -18,7 +18,24 @@ function openModal() {
     document.getElementById('modalOverlay').style.display = 'block';
   }
 
-  function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-    document.getElementById('modalOverlay').style.display = 'none';
-  }
+function closeModal() {
+  document.getElementById('modal').style.display = 'none';
+  document.getElementById('modalOverlay').style.display = 'none';
+}
+
+function completeTask(){
+  // change the icon to a filled radio button
+  // move the task to 'completed'
+}
+
+function createTask(){
+  // collect the info in the input and assign them to variables
+  var taskName = document.getElementById('taskName').innerText;
+  var taskDescription = document.getElementById('taskDescription').innerText;
+  var dueDate = document.getElementById('dueDate').innerText;
+  console.log(taskName);
+  console.log(taskDescription);
+  console.log(dueDate);
+  //push the content in the variables to the db
+  //show the contents in the db on the main screen -> showList()?
+}
