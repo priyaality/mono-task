4a612391eabe3a0f070073bf97c1b835d2c64abe
Priya Krishnan <spriyak02@gmail.com>
2025-02-10 11:00:34 +0530

    ref material to process form input

--- script.js
+++ script.js
@@ -39,3 +39,37 @@ function createTask(){
   //push the content in the variables to the db
   //show the contents in the db on the main screen -> showList()?
 }
+
+// This is to process/store form data in an object, presumably every field input from the form can be accessed by saying formData.name, formData.dueDate, etc..
+//ref link: https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Forms/Sending_forms_through_JavaScript
+/*
+async function sendData(data) {
+  // Construct a FormData instance
+  const formData = new FormData();
+
+  // Add a text field
+  formData.append("name", "Pomegranate");
+
+  // Add a file
+  const selection = await window.showOpenFilePicker();
+  if (selection.length > 0) {
+    const file = await selection[0].getFile();
+    formData.append("file", file);
+  }
+
+  try {
+    const response = await fetch("https://example.org/post", {
+      method: "POST",
+      // Set the FormData instance as the request body
+      body: formData,
+    });
+    console.log(await response.json());
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const send = document.querySelector("#send");
+send.addEventListener("click", sendData);
+
+*/
