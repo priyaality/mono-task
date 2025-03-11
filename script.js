17f4307f58609a658893fcb05b12aef98ef4ff41
Priya Krishnan <spriyak02@gmail.com>
2025-03-11 11:24:01 +0530

    been a while, updating the repo jic

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// import {Low} from "lowdb";
-// import { JSONFile } from "lowdb/node";
-// import lodash from "lodash";
-
-// const defaultData = {
-//     users: [], //for user info
-//     tasks: [], //for all the tasks
-//     lists: [], //for the lists that a task would be categorised into
-//     tags: [], //for tags that would be assigned to a task
-//     taskTags: [] //junction table for the many to many relationships between tags and tasks
-// }
-
-// const adapter = new JSONFile('db.json');
-// const db = new Low(adapter, defaultData);
-
 function openModal() {
     document.getElementById('modal').style.display = 'flex';
     document.getElementById('modalOverlay').style.display = 'block';
@@ -32,7 +17,8 @@ function closeModal() {
       description: document.getElementById('description').value,
       dueDate: document.getElementById('dueDate').value,
     };
-    console.log('Form submitted:', formData);
+    // console.log('Form submitted:', formData);
+    //store the data to backend
     closeModal();
   });
 
